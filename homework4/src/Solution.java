5e98f4845238d2a27589af13f1047508ab8bb305
Sergey Volkov <rusobraz@mail.ru>
2024-11-22 19:24:43 +0300

    Решение ДЗ№4

---
+++ homework4/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+    private static final int NUM_THREE = 3;
+    private static final int NUM_FIVE = 5;
+    private static final String TEXT_THREE = "fizz";
+    private static final String TEXT_FIVE = "buzz";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.print("0");
+        for (int i = 1; i <= n; i++) {
+            if (i % NUM_THREE == 0 && i % NUM_FIVE == 0) {
+                System.out.print(" " + TEXT_THREE + TEXT_FIVE);
+            } else if (i % NUM_THREE == 0) {
+                System.out.print(" " + TEXT_THREE);
+            } else if (i % NUM_FIVE == 0) {
+                System.out.print(" " + TEXT_FIVE);
+            } else {
+                System.out.print(" " + i);
+            }
+        }
+        scanner.close();
+    }
+}
