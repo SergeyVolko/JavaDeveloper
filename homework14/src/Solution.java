a938b8a810a09b1cedebfdc77059d80f4259b446
Sergey Volkov <rusobraz@mail.ru>
2024-11-26 11:38:35 +0300

    Решение ДЗ№14

---
+++ homework14/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(List.of(1, 2, 3, 5));
+        IntStream.rangeClosed(0, arr.size() + 1)
+                .forEach((e) -> System.out.println(e +": " + getRollingAverage(arr, e)));
+    }
+
+    public static List<Double> getRollingAverage(ArrayList<Integer> arr, int k) {
+        List<Double> list = new ArrayList<>();
+        int size = arr.size();
+        if (k <= 0 || k > size) {
+            return list;
+        }
+        for (int i = 0; i <= size - k; i++) {
+            int sum = 0;
+            int j;
+            for (j = i; j < size && j < k + i ; j++) {
+                sum += arr.get(j);
+            }
+            list.add((double) sum / (j - i));
+        }
+        return list;
+    }
+}
