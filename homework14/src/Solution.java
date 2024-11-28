c8031e6d0276f4318a71ba8e3e1ae9aecad09247
Sergey Volkov <rusobraz@mail.ru>
2024-11-28 11:31:00 +0300

    ## Коллекции: ArrayList и LinkedList

--- homework14/src/Solution.java
+++ homework14/src/Solution.java
@@ -9,12 +9,16 @@ public class Solution {
         System.out.println("Исходный массив: " + arr);
         IntStream.rangeClosed(0, arr.size() + 1)
                 .forEach((e) -> System.out.println(e +": " + getRollingAverage(arr, e)));
+        System.out.println("-".repeat(50));
+        System.out.println("Исходный массив: " + arr);
+        IntStream.rangeClosed(0, arr.size() + 1)
+                .forEach((e) -> System.out.println(e +": " + getRollingAverageWithWindow(arr, e)));
     }
 
     public static List<Double> getRollingAverage(ArrayList<Integer> arr, int k) {
         List<Double> list = new ArrayList<>();
         int size = arr.size();
-        if (k <= 0) {
+        if (k <= 0 || k > size) {
             return list;
         }
         for (int i = 0; i <= size - k; i++) {
@@ -24,6 +28,24 @@ public class Solution {
                 sum += arr.get(j);
             }
             list.add((double) sum / (j - i));
+        }
+        return list;
+    }
+
+    public static List<Double> getRollingAverageWithWindow(ArrayList<Integer> arr, int k) {
+        List<Double> list = new ArrayList<>();
+        int size = arr.size();
+        if (k <= 0 || k > size) {
+            return list;
+        }
+        double sum =0;
+        for (int i = 0; i < k; i++) {
+            sum += arr.get(i);
+        }
+        list.add(sum / k);
+        for (int i = 1; i < size - k; i++) {
+            sum = sum - arr.get(i - 1) + arr.get(i + k);
+            list.add(sum / k);
         }
         return list;
     }
